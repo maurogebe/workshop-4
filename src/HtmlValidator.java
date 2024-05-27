10590bcd0df66310c6c3b3c9a2a4abcd7b4c2c21
Mauricio <maurogebe.96@gmail.com>
2024-05-27 09:19:12 -0500

    Finish

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -7,10 +8,25 @@ public class HtmlValidator {
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
 
 		/* IMPLEMENT THIS METHOD! */
+		Stack<HtmlTag> openTags = new Stack<>();
 
-		return null; // this line is here only so this code will compile if you don't modify it
+		for (HtmlTag tag : tags) {
+			if(tag.isSelfClosing()) {
+			} else if(tag.isOpenTag()) {
+				openTags.add(tag);
+			} else {
+				if(!openTags.isEmpty() && openTags.peek().matches(tag)) {
+					openTags.pop();
+				} else {
+					if(openTags.isEmpty()) openTags.push(tag);
+					break;
+				}
+			}
+		}
+
+		if(!openTags.isEmpty() && !openTags.peek().isOpenTag()) return null;
+
+		return openTags; // this line is here only so this code will compile if you don't modify it
 	}
-	
-
 }
 
